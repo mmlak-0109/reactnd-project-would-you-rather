abe972d8f81b1a767d73f3beaf9fceb13776884e
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-09 23:36:35 +0900

    Connect to store and add componentDidMount

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom' 
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
 import NavBar from './NavBar';
 import SignIn from './SignIn';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+
   render() {
     return (
       <Router>
@@ -16,4 +23,4 @@ class App extends Component {
   }
 };
 
-export default App;
+export default connect()(App);
