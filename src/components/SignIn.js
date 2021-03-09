a1d44a4a9698575db5823b468c73ba4436614592
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-09 23:37:03 +0900

    Connect to store and flesh out UI

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,27 +1,42 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux'
+import { FaUserCircle } from "react-icons/fa";
 
 class SignIn extends Component {
   render() {
+    const { users } = this.props;
+    console.log(users)
     return (
-      <div>
-        <div>
+      <div className='container center'>
+        <div className='sign-in'>
           <h3>Welcome to the Would you Rather App!</h3>
           <p>Please sign in to continue</p>
         </div>
-        <div>
-          Sign In
-          <form>
-            <select>
-              <option value='user1'>user1</option>
-              <option value='user2'>user2</option>
-              <option value='user3'>user3</option>
-            </select>
-          </form>
-          <button>Sign In</button>
+        <div className='sign-in'>
+          <div>
+            <FaUserCircle size="10em"/>
+            <h2>Sign In</h2>
+          </div>
+          <div>
+            <form>
+              <select className='user-select'>
+                {users.map((user) => (
+                  <option id={user}>{user}</option>
+                ))}
+              </select>
+            </form>
+            <button className='btn'>Sign In</button>
+          </div>
         </div>
       </div>
     )
   }
 };
 
-export default SignIn;
+function mapStateToProps({ users }) {
+  return {
+      users: Object.keys(users)
+  }
+}
+
+export default connect(mapStateToProps)(SignIn);
