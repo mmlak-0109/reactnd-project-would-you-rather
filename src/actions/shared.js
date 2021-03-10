728af85495d0f930d0b8a4ccce27c248c70d36ef
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-10 23:12:44 +0900

    Add authedUser dispatch

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,17 +1,18 @@
 import { getInitialData } from '../utils/api';
+import { setAuthedUser } from './authedUser';
 import { receiveQuestions } from "./questions";
 import { receiveUsers } from './users';
 
-// const AUTHED_ID = 'tylermcginnis'
+const AUTHED_ID = null
 
 export function handleInitialData () {
     return (dispatch) => {
         // dispatch(showLoading())
         return getInitialData()
-            .then(({users, tweets}) => {
-                dispatch(receiveQuestions(tweets))
+            .then(({users, questions}) => {
+                dispatch(receiveQuestions(questions))
                 dispatch(receiveUsers(users))
-                // dispatch(setAuthedUser(AUTHED_ID))
+                dispatch(setAuthedUser(AUTHED_ID))
                 // dispatch(hideLoading())
             })
     }
