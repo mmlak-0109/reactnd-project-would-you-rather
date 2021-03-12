86c94a5e2e52df75954c927700cdde0447ad4a3e
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-13 00:22:53 +0900

    Create component to display individual questions

---
+++ src/components/QuestionPreview.js
@@ -0,0 +1,48 @@
+import { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { formatQuestionPreview } from "../utils/helpers";
+
+
+class QuestionPreview extends Component {
+  render() {
+    const { question } = this.props;
+
+    if (question === null) {
+      return <p>Sorry, this question doesn't exist.</p>
+    }
+
+    const { id, name, avatar, title, text } = question;
+
+    return (
+      <div className='question'>
+        <div>
+          <h3>{name} asks:</h3>
+        </div>
+        <img
+          className='avatar'
+          src={avatar}
+          alt='User Avatar'
+        />
+        <div className='question-info'>
+          <h4>{title}</h4>
+          <p>{text}...</p>
+          <button className='btn'>View Question</button>
+        </div>
+      </div>
+    )
+  }
+};
+
+function mapStateToProps({ questions, users }, { id }) {
+  const question = questions[id];
+  const user = users[question.author];
+
+  return {
+    question: question
+      ? formatQuestionPreview(question, user)
+      : null
+  }
+};
+
+export default connect(mapStateToProps)(QuestionPreview);
