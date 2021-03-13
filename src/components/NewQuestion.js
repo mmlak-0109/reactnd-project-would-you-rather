8aa92eb5d1bf429ab6d454535ad57bca1f536303
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-13 23:31:33 +0900

    Create NewQuestion component

---
+++ src/components/NewQuestion.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router";
+import { handleAddQuestion } from "../actions/questions";
+
+
+class NewQuestion extends Component {
+  state = {
+    optionOne: '',
+    optionTwo: '',
+    toHome: false
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+
+    const value = e.target.value;
+    const name = e.target.name;
+
+    if (name === 'optionOne') {
+      this.setState(() => ({
+        optionOne: value
+      }))
+    } else {
+      this.setState(() => ({
+        optionTwo: value
+      }))
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    const { optionOne, optionTwo } = this.state
+    const { dispatch } = this.props
+
+    dispatch(handleAddQuestion(optionOne, optionTwo))
+
+    this.setState(() => ({
+      optionOne: '',
+      optionTwo: '',
+      toHome: true
+    }))
+  }
+
+  render() {
+    const { optionOne, optionTwo, toHome } = this.state
+    console.log(this.state)
+
+    if (toHome === true) {
+      return <Redirect to='/home' />
+    }
+
+    return (
+      <div className='container'>
+        <div className='new-question center'>
+          <h2>Create New Question</h2>
+        </div>
+        <div className='new-question'>
+          <p>Complete the question</p>
+          <h4>Would you rather...</h4>
+          <form>
+            <input 
+              placeholder='Enter Option One Text Here'
+              value={optionOne}
+              name='optionOne'
+              onChange={this.handleChange} />
+            <h4 className='center'>OR</h4>
+            <input 
+              placeholder='Enter Option Two Text Here'
+              value={optionTwo}
+              name='optionTwo'
+              onChange={this.handleChange} />
+            <button 
+              className='btn'
+              onSubmit={this.handleSubmit}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+
+    )
+  }
+}
+
+export default connect()(NewQuestion)
