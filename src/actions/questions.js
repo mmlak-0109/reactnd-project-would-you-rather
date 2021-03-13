387ed4a0a6bac350dc564d67961b2d525401ba64
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-13 23:28:05 +0900

    Added action for adding a new question

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,31 @@
-export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+import { saveQuestion } from "../utils/api";
 
-export function receiveQuestions (questions) {
-    return {
-        type: RECEIVE_QUESTIONS,
-        questions,
-    }
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+export const ADD_QUESTION = 'ADD_QUESTION';
+
+export function receiveQuestions(questions) {
+  return {
+    type: RECEIVE_QUESTIONS,
+    questions,
+  }
+};
+
+function addQuestion(question) {
+  return {
+    type: ADD_QUESTION,
+    question,
+  }
+};
+
+export function handleAddQuestion(optionOne, optionTwo) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+
+    return saveQuestion({
+      optionOneText: optionOne,
+      optionTwoText: optionTwo,
+      author: authedUser
+    })
+      .then((question) => dispatch(addQuestion(question)))
+  }
 }
