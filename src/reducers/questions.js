f90ed6f33d7660adaeeea4d0aa13974985996141
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-13 23:34:12 +0900

    Create "ADD_QUESTION" case

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,13 +1,18 @@
-import { RECEIVE_QUESTIONS } from '../actions/questions'
+import { ADD_QUESTION, RECEIVE_QUESTIONS } from '../actions/questions'
 
-export default function questions (state = {}, action) {
-    switch (action.type) {
-        case RECEIVE_QUESTIONS:
-            return {
-                ...state,
-                ...action.questions
-            }
-        default:
-            return state
-    }
+export default function questions(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_QUESTIONS:
+      return {
+        ...state,
+        ...action.questions
+      }
+    case ADD_QUESTION:
+      return {
+        ...state,
+        ...action.question
+      }
+    default:
+      return state
+  }
 }
