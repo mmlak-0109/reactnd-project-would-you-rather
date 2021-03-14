0324e87dc483d479aefaf636ffdd7c35b0ce7d36
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-14 23:05:30 +0900

    Fix input overflow and change styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
-export default function NavBar() {
-  return (
-    <nav className='nav'>
-      <ul>
-        <li>
-          <NavLink to='/home' exact activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/new-question' activeClassName='active'>
-            New Question
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/leaderboard' activeClassName='active'>
-            Leaderboard
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  )
+class NavBar extends Component {
+  render() {
+    const { authedUser } = this.props;
+    return (
+      <nav className='nav'>
+        <ul>
+          <li>
+            <NavLink 
+              to={
+                authedUser === null
+                ? '/sign-in'
+                : '/home'}
+              activeClassName={
+                authedUser === null
+                ? ''
+                : 'active'}>
+                Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+              to={
+                authedUser === null
+                ? '/sign-in'
+                : '/new-question'}
+              activeClassName={
+                authedUser === null
+                ? ''
+                : 'active'}>
+                New Question
+            </NavLink>
+          </li>
+          <li>
+            <NavLink 
+              to={
+                authedUser === null
+                ? '/sign-in'
+                : '/leaderboard'}
+              activeClassName={
+                authedUser === null
+                ? ''
+                : 'active'}>
+                Leaderboard
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
 };
+
+function mapStateToProps(state) {
+  return {
+    authedUser: state.authedUser
+  }
+}
+
+export default connect(mapStateToProps)(NavBar)
