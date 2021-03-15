5c5169886f6bc81c6170e098ae2946a2b7d3a0f2
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-15 23:45:54 +0900

    Stop setting authedUser at initial data load

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,9 +1,6 @@
 import { getInitialData } from '../utils/api';
-import { setAuthedUser } from './authedUser';
 import { receiveQuestions } from "./questions";
 import { receiveUsers } from './users';
-
-const AUTHED_ID = null
 
 export function handleInitialData () {
     return (dispatch) => {
@@ -12,7 +9,6 @@ export function handleInitialData () {
             .then(({users, questions}) => {
                 dispatch(receiveQuestions(questions))
                 dispatch(receiveUsers(users))
-                dispatch(setAuthedUser(AUTHED_ID))
                 // dispatch(hideLoading())
             })
     }
