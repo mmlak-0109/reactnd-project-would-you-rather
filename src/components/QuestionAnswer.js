1e0e84904339ce0a6a13d732e86d9bee6404cebd
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-19 23:26:36 +0900

    Renamed Question component to QuestionAnswer

---
+++ src/components/QuestionAnswer.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import { connect } from "react-redux";
+
+
+class QuestionAnswer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
+export default connect()(QuestionAnswer)
