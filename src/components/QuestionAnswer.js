a309a06293bc65cf25ecf759195875103476e599
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-22 23:36:13 +0900

    Add code to get props and check toggle conditional

--- src/components/QuestionAnswer.js
+++ src/components/QuestionAnswer.js
@@ -1,16 +1,30 @@
 import { Component } from "react";
 import { connect } from "react-redux";
+import Question from "./Question";
+import Results from "./Results";
 
 
 class QuestionAnswer extends Component {
   render() {
+    const { question } = this.props.question;
+    const toggle = this.props.location.state.toggle;
+    console.log(toggle)
     return (
       <div>
-        
+        {toggle === 'unanswered'
+          ? <Question questionInfo={question}/>
+          : <Results questionInfo={question}/>}
       </div>
     )
   }
 }
 
+function mapStateToProps({ questions }, props) {
+  const { id } = props.match.params;
 
-export default connect()(QuestionAnswer)
+  return{
+    question: questions[id],
+  }
+}
+
+export default connect(mapStateToProps)(QuestionAnswer)
