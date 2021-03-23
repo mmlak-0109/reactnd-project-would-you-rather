ae0c075b9bc94cfa9d3667a70012a78ca17a0b3b
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-23 23:53:33 +0900

    Add function to format question

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10,3 +10,17 @@ export function formatQuestionPreview(question, user) {
     text: optionOne['text']
   }
 }
+
+export function formatQuestion(question, user) {
+  const { id, optionOne, optionTwo} = question;
+  const { name, avatarURL } = user;
+
+  return {
+    id,
+    name,
+    avatar: avatarURL,
+    title: 'Would you rather...',
+    optionOneText: optionOne['text'],
+    optionTwoText: optionTwo['text']
+  }
+}
