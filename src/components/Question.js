65432709211497ff037ab62c41c90e9bcf063e26
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-24 00:04:44 +0900

    Create UI, mapStateToProps, local state

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,71 @@
 import { Component } from "react";
+import { FaUserCircle } from "react-icons/fa";
 import { connect } from "react-redux";
+import { formatQuestion } from "../utils/helpers";
 
 
 class Question extends Component {
+  state = {
+    selected: ""
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      selected: e.target.value
+    });
+  }
+
   render() {
+    const { id, name, avatar, title, optionOneText, optionTwoText} = this.props.question;
+    const { selected } = this.state;
+
     return (
-      <div>
-        Question
+      <div className='question'>
+        {/* <img
+          className='avatar'
+          src={avatar}
+          alt='User Avatar'
+          width='50%'
+        /> */}
+          <FaUserCircle size='10em' />
+        <div className='question-info'>
+          <h3>{name} asks:</h3>
+          <h4>{title}</h4>
+          <div>
+            <label className='radio-buttons'>
+              <input 
+                type='radio'
+                value={optionOneText}
+                checked={selected === {optionOneText}}
+                onChange={this.handleChange}/>
+              {optionOneText}
+            </label>
+            <label className='radio-buttons'>
+              <input 
+                type='radio' 
+                value={optionTwoText}
+                checked={selected === {optionTwoText}}
+                onChange={this.handleChange}/>
+              {optionTwoText}
+            </label>
+          </div>
+          <button className='btn' onClick={this.handleClick}>
+            Submit
+          </button>
+        </div>
       </div>
     )
   }
 }
 
-export default connect()(Question)
+function mapStateToProps({ users, authedUser }, { questionInfo }) {
+  const user = users[questionInfo.author];
+
+  return {
+    authedUser,
+    question: formatQuestion(questionInfo, user)
+  }
+}
+export default connect(mapStateToProps)(Question)
