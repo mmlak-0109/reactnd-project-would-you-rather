ca4ce3539ea59c93a9a348a1870b91ae8780ca20
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-25 23:46:28 +0900

    Add formatResults function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -24,3 +24,19 @@ export function formatQuestion(question, user) {
     optionTwoText: optionTwo['text']
   }
 }
+
+export function formatResults(question, user) {
+  const { id, optionOne, optionTwo} = question;
+  const { name, avatarURL } = user;
+
+  return {
+    id,
+    name,
+    avatar: avatarURL,
+    title: 'Would you rather',
+    optionOneText: optionOne['text'],
+    optionTwoText: optionTwo['text'],
+    optionOneVotes: optionOne['votes'],
+    optionTwoVotes: optionTwo['votes']
+  }
+}
