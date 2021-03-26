78edbb89688ff10f64d991eb724b34124b9a66db
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-26 23:06:35 +0900

    Add autheduser, optionsObject, and map to add component

--- src/components/Results.js
+++ src/components/Results.js
@@ -17,6 +17,8 @@ class Results extends Component {
       optionTwoVotes
     } = this.props.question;
 
+    const authedUser = this.props.authedUser;
+
     const optionOneVotesLen = optionOneVotes.length;
     const optionTwoVotesLen = optionTwoVotes.length;
     const totalVotes = optionOneVotesLen + optionTwoVotesLen;
@@ -46,9 +48,28 @@ class Results extends Component {
       textAlign: 'right'
     }
 
+    const optionsObject = {
+      'optionOne': { 
+        text: optionOneText, 
+        bar: optionOneBar,
+        pct: optionOneVotesPct,
+        length: optionOneVotesLen,
+        votes: optionOneVotes
+      },
+      'optionTwo': { 
+        text: optionTwoText, 
+        bar: optionTwoBar,
+        pct: optionTwoVotesPct,
+        length: optionTwoVotesLen,
+        votes: optionTwoVotes
+      }
+    }
+
+    const optionsList = ['optionOne', 'optionTwo']
+
     return (
       <div className='results-container'>
-        <h2 className='center'>Results:</h2>
+        <h2>Results:</h2>
         <div className='results'>
           {/* <img
             className='avatar'
@@ -57,30 +78,29 @@ class Results extends Component {
             width='50%'
           /> */}
             <FaUserCircle size='10em' />
-          <div >
-            <div className='results-info'>
-              <h3>{name} asked:</h3>
-              <div>
-                <p>{title} {optionOneText}?</p>
-                <div style={barContainer}>
-                  <div style={optionOneBar}>
-                    <span>{`${optionOneVotesPct}%`}</span>
+          <div className='center'>
+            <h3>{name} asked:</h3>
+            <ul>
+              {optionsList.map(option => (
+                <li key={option}>
+                  <div className='results-info'>
+                    <div>
+                      {optionsObject[option].votes.includes(authedUser)
+                          ? <div className='your-selection'>Your Selection</div>
+                          : null
+                        }
+                      <p>{title} {optionsObject[option].text}?</p>
+                      <div style={barContainer}>
+                        <div style={optionsObject[option].bar}>
+                          <span>{`${optionsObject[option].pct}%`}</span>
+                        </div>
+                      </div>
+                      <p className='center'>{optionsObject[option].length} out of {totalVotes} Votes</p>
+                    </div>
                   </div>
-                </div>
-                <p className='center'>{optionOneVotesLen} out of {totalVotes} Votes</p>
-              </div>
-            </div>
-            <div className='results-info'>
-              <div>
-                <p>{title} {optionTwoText}?</p>
-                <div style={barContainer}>
-                  <div style={optionTwoBar}>
-                    <span>{`${optionTwoVotesPct}%`}</span>
-                  </div>
-                </div>
-                <p className='center'>{optionTwoVotesLen} out of {totalVotes} Votes</p>
-              </div>
-            </div>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
