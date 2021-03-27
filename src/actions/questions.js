9c4ec9f218791adbb33427a6f651d8f4cd5de36b
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-27 22:22:53 +0900

    Add functions to add question answers

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,7 +1,9 @@
-import { saveQuestion } from "../utils/api";
+import { saveQuestion, saveQuestionAnswer } from "../utils/api";
+import { addAnswerToUser, addQuestionToUser } from "./users";
 
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
 export const ADD_QUESTION = 'ADD_QUESTION';
+export const ADD_QUESTION_ANSWER = "ADD_QUESTION_ANSWER"
 
 export function receiveQuestions(questions) {
   return {
@@ -26,6 +28,36 @@ export function handleAddQuestion(optionOne, optionTwo) {
       optionTwoText: optionTwo,
       author: authedUser
     })
-      .then((question) => dispatch(addQuestion(question)))
+      .then((question) => {
+        dispatch(addQuestion(question));
+        dispatch(addQuestionToUser({question, authedUser}))
+      }
+    )
+  }
+}
+
+function addQuestionAnswer({ authedUser, qid, answer }) {
+  return {
+    type: ADD_QUESTION_ANSWER,
+    authedUser,
+    qid,
+    answer
+  }
+};
+
+export function handleAddQuestionAnswer(id, option) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+
+    return saveQuestionAnswer({
+      authedUser,
+      qid: id,
+      answer: option
+    })
+      .then((answerToReturn) => {
+        dispatch(addQuestionAnswer(answerToReturn));
+        dispatch(addAnswerToUser(answerToReturn))
+      }
+    )
   }
 }
