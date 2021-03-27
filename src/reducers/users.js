f4aa4539038d9be8dd53edba7459e144556c5087
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-27 22:18:01 +0900

    Add cases for adding questions and answers to user

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,4 @@
-import { RECEIVE_USERS } from '../actions/users'
+import { ADD_ANSWER_TO_USER, ADD_QUESTION_TO_USER, RECEIVE_USERS } from '../actions/users'
 
 export default function users (state = {}, action) {
     switch (action.type) {
@@ -6,6 +6,27 @@ export default function users (state = {}, action) {
             return {
                 ...state,
                 ...action.users
+            }
+        case ADD_QUESTION_TO_USER:
+            console.log(action.authedUser)
+            console.log(action.id)
+            return {
+                ...state,
+                [action.authedUser]: {
+                    ...state[action.authedUser],
+                    questions: state[action.authedUser].questions.concat([action.id])
+                  }
+            }
+        case ADD_ANSWER_TO_USER:
+            return {
+                ...state,
+                [action.authedUser]: {
+                    ...state[action.authedUser],
+                    answers: {
+                      ...state[action.authedUser].answers,
+                      [action.qid]: action.answer
+                    }
+                  }
             }
         default:
             return state
