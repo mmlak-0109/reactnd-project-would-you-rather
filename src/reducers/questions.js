f7cb31d3108dee931a350ab5e7c95c7739327b86
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-27 22:18:26 +0900

    Add case to add answer

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,4 @@
-import { ADD_QUESTION, RECEIVE_QUESTIONS } from '../actions/questions'
+import { ADD_QUESTION, ADD_QUESTION_ANSWER, RECEIVE_QUESTIONS } from '../actions/questions'
 
 export default function questions(state = {}, action) {
   switch (action.type) {
@@ -11,6 +11,17 @@ export default function questions(state = {}, action) {
       return {
         ...state,
         [action.question.id]: action.question
+      }
+    case ADD_QUESTION_ANSWER:
+      return {
+        ...state,
+        [action.qid]: {
+          ...state[action.qid],
+          [action.answer]: {
+            ...state[action.qid][action.answer],
+            votes: state[action.qid][action.answer].votes.concat([action.authedUser])
+          }
+        }
       }
     default:
       return state
