1ddfdf47beb4d3138b3cfc5aa7486ca91d52ef41
Matthew Miller <mmlak_0109@yahoo.com>
2021-03-27 22:21:34 +0900

    Add functions to add questions and answers to user

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,28 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS'
+export const ADD_ANSWER_TO_USER = 'ADD_ANSWER_TO_USER';
+export const ADD_QUESTION_TO_USER = 'ADD_QUESTION_TO_USER'
 
 export function receiveUsers (users) {
     return {
         type: RECEIVE_USERS,
         users,
+    }
+}
+
+export function addQuestionToUser ({ question, authedUser }) {
+    return {
+        type: ADD_QUESTION_TO_USER,
+        id: question.id,
+        authedUser
+    }
+}
+
+export function addAnswerToUser ({ authedUser, qid, answer }) {
+    console.log({ authedUser, qid, answer })
+    return {
+        type: ADD_ANSWER_TO_USER,
+        qid,
+        answer,
+        authedUser
     }
 }
