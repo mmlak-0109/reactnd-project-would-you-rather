d5f9504f7e6bbe1bc336ab862ade7ed6a089b467
Matthew Miller <mmlak_0109@yahoo.com>
2021-04-01 00:00:58 +0900

    Add logic to catch errors and redirect to error page

--- src/components/QuestionAnswer.js
+++ src/components/QuestionAnswer.js
@@ -2,11 +2,20 @@ import { Component } from "react";
 import { connect } from "react-redux";
 import Question from "./Question";
 import Results from "./Results";
+import { Redirect } from "react-router";
 
 
 class QuestionAnswer extends Component {
   render() {
+
+    
     const { question } = this.props;
+
+    if (this.props.location.state === undefined
+        || question === undefined) {
+      return <Redirect to='/no-match' />
+    }
+
     const toggle = this.props.location.state.toggle;
 
     return (
